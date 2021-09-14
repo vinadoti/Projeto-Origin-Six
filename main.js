0f3b5bdaa0b74322d1cab2919f59f11d76e10130
vinadoti <vinadoti@gmail.com>
2021-09-13 23:10:16 -0300

    Final da quarta aula

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links){
 }
 
 //mudar o header da página quando der scroll
-const header = document.querySelector("#header")
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll',function (){
+function changeHeaderWhenScroll(){
+  const header = document.querySelector("#header")
+  const navHeight = header.offsetHeight
+  
   if(window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -29,7 +29,9 @@ window.addEventListener('scroll',function (){
     // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
+
+
 
 // Testimonials swiper (carrosel)
 const swiper = new Swiper('.swiper',{
@@ -53,8 +55,25 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/* Botão voltar para o topo */
+function backToTop () {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if(window.scrollY >=560){
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+})
